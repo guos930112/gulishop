6026f728740564f6054b91b57d68cf631662cece
guos <guos930112@betaex.com>
2020-05-15 21:03:27 +0800

    add cancel userfav

--- apps/operations/views.py
+++ apps/operations/views.py
@@ -5,6 +5,8 @@ from .serializers import UserFavSerializer
 # Create your views here.
 
 
-class UserFavViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+class UserFavViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = UserFav.objects.all()
     serializer_class = UserFavSerializer
+    # 登陆权限/ 操作 收藏的权限
+    lookup_field = 'goods_id'  # 我们要把商品id给到前端 但是 不配置的话返回的是 userfav对象的id 和前端不匹配，前端是商品id
