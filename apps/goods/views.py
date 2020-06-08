672e559517c0b9255cc0acf89f6502bef9504709
guos <guos930112@betaex.com>
2020-06-08 22:12:24 +0800

    add click_num statistic

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -95,6 +95,13 @@ class GoodsViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.Ge
     search_fields = ('name', 'desc', 'goods_brief')
     ordering_fields = ('shop_price', 'market_price')
 
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.click_num += 1
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
 
 class CategoryViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     # queryset = GoodsCategory.objects.all()
